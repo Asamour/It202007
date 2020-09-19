b0abb8d3d66fe6791b36e0925d6f9cdc56016c7a
Asamour <adhamsamour6@gmail.com>
2020-09-19 12:29:59 -0400

    first homework assignment for loops

--- homework1.php
+++ homework1.php
@@ -1,4 +1,10 @@
 <?php
-    $number = range(0,5);
-        echo $number;
+    $number = array(1, 2, 3, 4, 5);
+    foreach ($number as $val) {
+        if($val%2==0)
+        {
+            echo "$val \n";
+        }
     }
+    
+?>
